0a72e94bf30777b72593498606332e8511f27209
Moritz Schneider <schneidermoritz90@gmail.com>
2017-05-18 19:32:43 +0200

    Sets up DB testing

---
+++ tests/db_test/main.go
@@ -0,0 +1,39 @@
+// +build !test
+package db_test
+
+import (
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+	_ "github.com/lib/pq"
+	"github.com/scmo/foodchain-backend/db"
+)
+
+func Setup() {
+	beego.Info("Initialize Database")
+	// Register Driver
+	orm.RegisterDriver("postgres", orm.DRPostgres)
+
+	dataSource := "user=postgres password=test123456 dbname=db_foodchain_test sslmode=disable"
+
+	beego.Info(dataSource)
+
+	// set default database
+	err := orm.RegisterDataBase("default", "postgres", dataSource, 30, 30)
+	// Error.
+	err = orm.RunSyncdb("default", true, false)
+	if err != nil {
+		beego.Error("RunSyncdb Error")
+	}
+	// Log every SQL Query
+	orm.Debug = false
+
+	// Populate DB
+	db.Seed_LegalForm()
+	db.Seed_PlantType()
+
+	db.Seed_Contributions()
+	db.Seed_ControlPoints()
+}
+
+
+
