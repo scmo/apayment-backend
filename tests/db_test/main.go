b9c4f06f018ea89e35c10a6aa5493e46f22ffb93
Moritz Schneider <schneidermoritz90@gmail.com>
2017-06-05 10:24:59 +0200

    project renaming: rename database

--- tests/db_test/main.go
+++ tests/db_test/main.go
@@ -13,8 +13,8 @@ func Setup() {
 	// Register Driver
 	orm.RegisterDriver("postgres", orm.DRPostgres)
 
-	dataSource := "port=9032 user=postgres password=test123456 dbname=db_apayment_test sslmode=disable"
-	//dataSource := "user=postgres password=test123456 dbname=db_apayment_test sslmode=disable"
+	//dataSource := "port=9032 user=postgres password=test123456 dbname=db_apayment_test sslmode=disable"
+	dataSource := "user=postgres password=test123456 dbname=db_apayment_test sslmode=disable"
 
 	beego.Info(dataSource)
 
