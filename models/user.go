68a54cbc0945cb41c3bf917a9e699ec14d3dc4d5
Moritz Schneider <schneidermoritz90@gmail.com>
2017-06-23 17:30:45 +0200

    Clean up User Model

--- models/user.go
+++ models/user.go
@@ -3,28 +3,26 @@ package models
 import (
 	"github.com/astaxie/beego/orm"
 	"math/big"
+	"github.com/scmo/apayment-backend/services/tvd"
 )
 
 type User struct {
-	Id                   int64 `json:"id"`
-	Username             string  `orm:"unique" json:"username"`
-	Password             string  `json:"password"`
+	Id                          int64 `json:"id"`
+	Username                    string        `orm:"unique" json:"username"`
+	Password                    string        `json:"password"`
 
-	Roles                []*Role `orm:"rel(m2m)" json:"roles"`
-	JwtToken             string  `orm:"-" json:"token"`
-	EtherumAddress       string
-	EthereumBalance      *big.Int `orm:"-" json:"ethereumBalance"`
-	APaymentTokenBalance *big.Int`orm:"-" json:"apaymentTokenBalance"`
+	Roles                       []*Role        `orm:"rel(m2m)" json:"roles"`
+	JwtToken                    string        `orm:"-" json:"token"`
+	EtherumAddress              string                `json:"etherumAddress"`
+	EthereumBalance             *big.Int        `orm:"-" json:"ethereumBalance"`
+	APaymentTokenBalance        *big.Int        `orm:"-" json:"apaymentTokenBalance"`
 
-	Firstname            string  `json:"firstname"`
-	Lastname             string  `json:"lastname"`
-	Phone                string  `json:"phone"`
-	EMail                string  `json:"email"`
-	Address              string  `json:"address"`
-	ZIP                  uint16  `json:"zip"`
-	Place                string  `json:"place"`
-	Plant                *Plant `orm:"rel(one);null" json:"plant"`
-	TVD                  int32        `json: "tvd"`
+	Firstname                   string        `json:"firstname"`
+	Lastname                    string        `json:"lastname"`
+
+	PersonAddressResult         *tvd.PersonAddressResult `orm:"-" json:"personAddressResult"`
+	AnimalHusbandryDetailResult *tvd.GetAnimalHusbandryDetailResult  `orm:"-" json:"AnimalHusbandryDetailResult"`
+	TVD                         int32        `json: "tvd"`
 }
 
 func init() {
