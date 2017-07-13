92dd5714d961f75c3b4308aebc23ab5b35dcf81a
Moritz Schneider <schneidermoritz90@gmail.com>
2017-07-13 15:11:12 +0200

    First payment amount, final amount

--- smart-contracts/request.sol
+++ smart-contracts/request.sol
@@ -29,10 +29,10 @@ contract mortal {
 
 contract Request is mortal {
   address public inspectorAddress;
-
   uint16[] public contributionCodes;
-
   string public remark;
+
+  uint public amountPreviousYear;
 
   RBAC rbac;
 
@@ -50,11 +50,12 @@ contract Request is mortal {
   mapping (uint => Lack) public lacks;
 
 
-  function Request(uint16[] _contributionCodes, string _remark, address _rbacAddress, uint16[] gves) public {
+  function Request(uint16[] _contributionCodes, string _remark, address _rbacAddress, uint16[] _gves, uint _amountPreviousYear) public {
     rbac = RBAC(_rbacAddress);
     contributionCodes = _contributionCodes;
     remark = _remark;
-    setGVE(gves[0], gves[1], gves[2], gves[3], gves[4], gves[5], gves[6], gves[7], gves[8]);
+    setGVE(_gves[0], _gves[1], _gves[2], _gves[3], _gves[4], _gves[5], _gves[6], _gves[7], _gves[8]);
+    amountPreviousYear = _amountPreviousYear;
     setCreated();
   }
 
@@ -73,109 +74,122 @@ contract Request is mortal {
       if (_contributionCodes[i] == 5416) {
         updateBtsPoint(_pointGroupCodes[i], _points[i]);
       }
+      if (_contributionCodes[i] == 5417) {
+        updateRausPoint(_pointGroupCodes[i], _points[i]);
+      }
     }
     setModified();
   }
 
-  /* ==============================
-    Calculate Direct Payment amount
-    ===============================
-    - All amounts in rappen */
 
-  // 1110   Milchkühe
-  // 1150   andere Kühe
-
-  // 1128   weibliche Tiere über 365 - 730 Tage alt, ohne Abkalbung
-  // 1141   weibliche Tiere über 160 - 365 Tage alt
-  // 1142   weibliche Tiere bis 160 Tage alt (nur RAUS)
-  // 1124   männliche Tiere, über 730 Tage alt
-  // 1129   männliche Tiere, über 365 bis 730 Tage alt
-  // 1143   männliche Tiere, über 160 bis 365 Tage alt
-  // 1144   männliche Tiere, bis 160 Tage alt (nur RAUS)
-
-  // For each pointGroup there is one CalcVariables
+  // For each pointGroup there is a PointGroupCalculation
   uint16[9] pointGroupCodes = [1110, 1150, 1128, 1141, 1142, 1124, 1129, 1143, 1144];
 
-  mapping (uint16 => CalcVariables) public pointGroups;
 
-  struct CalcVariables {
+  struct PointGroupCalculation {
   uint16 gve;
-  uint16 points;
   uint16 btsPoints;
   uint256 btsTotal;
   uint256 btsDeduction;
+  uint16 rausPoints;
+  uint256 rausTotal;
+  uint256 rausDeduction;
   }
 
-  function setGVE(uint16 _gve1110,
-  uint16 _gve1150,
-  uint16 _gve1128,
-  uint16 _gve1141,
-  uint16 _gve1142,
-  uint16 _gve1124,
-  uint16 _gve1129,
-  uint16 _gve1143,
-  uint16 _gve1144) {
-    CalcVariables btsPointGroup = pointGroups[1110];
-    btsPointGroup.gve = _gve1110;
+  mapping (uint16 => PointGroupCalculation) public pointGroups;
 
+  function setGVE(uint16 _gve1110, uint16 _gve1150, uint16 _gve1128, uint16 _gve1141, uint16 _gve1142, uint16 _gve1124, uint16 _gve1129, uint16 _gve1143, uint16 _gve1144) {
+    PointGroupCalculation storage btsPointGroup = pointGroups[1110];
+    btsPointGroup.gve = _gve1110;
     btsPointGroup = pointGroups[1150];
     btsPointGroup.gve = _gve1150;
-
     btsPointGroup = pointGroups[1128];
     btsPointGroup.gve = _gve1128;
-
     btsPointGroup = pointGroups[1141];
     btsPointGroup.gve = _gve1141;
-
     btsPointGroup = pointGroups[1142];
     btsPointGroup.gve = _gve1142;
-
     btsPointGroup = pointGroups[1124];
     btsPointGroup.gve = _gve1124;
-
     btsPointGroup = pointGroups[1129];
     btsPointGroup.gve = _gve1129;
-
     btsPointGroup = pointGroups[1143];
     btsPointGroup.gve = _gve1143;
-
     btsPointGroup = pointGroups[1144];
     btsPointGroup.gve = _gve1144;
-
     calculateBTS();
     setModified();
   }
 
-  //  function getBTSGVE(uint16 _pointGroupCode) constant returns (uint16) {
-  //    return pointGroups[_pointGroupCode].gve;
-  //  }
 
   function updateBtsPoint(uint16 _pointGroupCode, uint16 _points) {
-    CalcVariables btsPointGroup = pointGroups[_pointGroupCode];
-    btsPointGroup.btsPoints = btsPointGroup.btsPoints + _points;
-    setModified();
+    PointGroupCalculation storage pointGroupCalculation = pointGroups[_pointGroupCode];
+    pointGroupCalculation.btsPoints = pointGroupCalculation.btsPoints + _points;
+  }
+
+  function updateRausPoint(uint16 _pointGroupCode, uint16 _points) {
+    PointGroupCalculation storage pointGroupCalculation = pointGroups[_pointGroupCode];
+    pointGroupCalculation.rausPoints = pointGroupCalculation.rausPoints + _points;
   }
 
   function calculateBTS() {
     for (uint16 i = 0; i < pointGroupCodes.length; i++) {
-      CalcVariables btsPointGroup = pointGroups[pointGroupCodes[i]];
-      //      btsPointGroup.btsTotal =  uint256(btsPointGroup.gve) * 9000;
+      PointGroupCalculation storage btsPointGroup = pointGroups[pointGroupCodes[i]];
+
       if ((pointGroupCodes[i] == 1142 || pointGroupCodes[i] == 1144) == false) {
-        if (btsPointGroup.btsPoints < 110) {
-          btsPointGroup.btsTotal = uint256(btsPointGroup.gve) * 9000;
-          //btsPointGroup.btsDeduction = (btsPointGroup.btsPoints - 10) * (9000 / 100);
+        btsPointGroup.btsTotal = uint256(btsPointGroup.gve) * 9000;
+        if (btsPointGroup.btsPoints == 0) {
+          continue;
         }
+        if (btsPointGroup.btsPoints > 110) {
+          btsPointGroup.btsDeduction = btsPointGroup.btsTotal;
+          continue;
+        }
+        btsPointGroup.btsDeduction = (btsPointGroup.btsPoints - 10) * (9000 / 100);
       }
+    }
+  }
+
+  function calculateRAUS() {
+    for (uint16 i = 0; i < pointGroupCodes.length; i++) {
+      PointGroupCalculation storage rausPointGroup = pointGroups[pointGroupCodes[i]];
+      uint256 multiplier = 0;
+      if (pointGroupCodes[i] == 1142 || pointGroupCodes[i] == 1144) {
+        multiplier = 37000;
+      }
+      else {
+        multiplier = 19000;
+      }
+      rausPointGroup.rausTotal = uint256(rausPointGroup.gve) * multiplier;
+      if (rausPointGroup.rausPoints == 0) {
+        continue;
+      }
+      if (rausPointGroup.rausPoints > 110) {
+        rausPointGroup.rausDeduction = rausPointGroup.rausTotal;
+        continue;
+      }
+      rausPointGroup.rausDeduction = (rausPointGroup.rausPoints - 10) * (multiplier / 100);
+
     }
   }
 
   function getFirstPaymentAmount() constant returns (uint256) {
     uint256 amount = 0;
-    for (uint16 i = 0; i < pointGroupCodes.length; i++) {
-      CalcVariables calcVar = pointGroups[pointGroupCodes[i]];
-      amount = amount + calcVar.btsTotal;
+    if (amountPreviousYear > 0) {
+      // first payment is 50% of amount of previous year
+      amount = amountPreviousYear / 2;
     }
     return amount;
   }
 
+  function getFinalPaymentAmount() constant returns (uint256){
+
+    uint256 amount = 0;
+    for (uint16 i = 0; i < pointGroupCodes.length; i++) {
+      PointGroupCalculation storage pointGroup = pointGroups[pointGroupCodes[i]];
+      amount += (pointGroup.btsTotal - pointGroup.btsDeduction);
+      amount += (pointGroup.rausTotal - pointGroup.rausDeduction);
+    }
+    return amount - getFirstPaymentAmount();
+  }
 }
--- tests/contracts/request/request_test.go
+++ tests/contracts/request/request_test.go
@@ -16,13 +16,21 @@ import (
 
 var farmerAuth *bind.TransactOpts
 var inspectorAuth *bind.TransactOpts
+var fakeInspectorAuth *bind.TransactOpts
 var adminAuth *bind.TransactOpts
+var cantonAuth *bind.TransactOpts
 var systemAuth *bind.TransactOpts
 var sim *backends.SimulatedBackend
 
 var rbacAddress common.Address
 var requestContract *directpaymentrequest.RequestContract
 
+var amountPreviousYear *big.Int
+
+/*
+ Prepare accounts and simulated backend
+ Deploy RBAC contract and add roles to address
+*/
 func init() {
 	// Generate a new random account and a funded simulator
 	key, _ := crypto.GenerateKey()
@@ -33,32 +41,47 @@ func init() {
 
 	key, _ = crypto.GenerateKey()
 	inspectorAuth = bind.NewKeyedTransactor(key)
+
+	key, _ = crypto.GenerateKey()
+	fakeInspectorAuth = bind.NewKeyedTransactor(key)
+
+	key, _ = crypto.GenerateKey()
+	cantonAuth = bind.NewKeyedTransactor(key)
+
 	key, _ = crypto.GenerateKey()
 	systemAuth = bind.NewKeyedTransactor(key)
 
 	sim = backends.NewSimulatedBackend(core.GenesisAlloc{
-		farmerAuth.From:    {Balance: big.NewInt(10000000000)},
-		adminAuth.From:     {Balance: big.NewInt(10000000000)},
-		systemAuth.From:    {Balance: big.NewInt(10000000000)},
-		inspectorAuth.From: {Balance: big.NewInt(10000000000)},
+		farmerAuth.From:        {Balance: big.NewInt(10000000000)},
+		adminAuth.From:         {Balance: big.NewInt(10000000000)},
+		systemAuth.From:        {Balance: big.NewInt(10000000000)},
+		inspectorAuth.From:     {Balance: big.NewInt(10000000000)},
+		cantonAuth.From:        {Balance: big.NewInt(10000000000)},
+		fakeInspectorAuth.From: {Balance: big.NewInt(10000000000)},
 	})
 	ra, _, rbacContract, _ := rbac.DeployRBACContract(systemAuth, sim)
 	sim.Commit()
 	rbacAddress = ra
 	rbacContract.AddFarmer(systemAuth, farmerAuth.From)
+	rbacContract.AddCantonEmployee(systemAuth, cantonAuth.From)
 	rbacContract.AddAdmin(systemAuth, adminAuth.From)
 	rbacContract.AddInspector(systemAuth, inspectorAuth.From)
+	rbacContract.AddInspector(systemAuth, fakeInspectorAuth.From)
 	sim.Commit()
+
+	amountPreviousYear = big.NewInt(500000)
 }
 
-func TestDeployContract(t *testing.T) {
+/*
+ Deploy DirectPaymentRequest
+*/
+func Test_DeployContract(t *testing.T) {
 	contributionCodes := []uint16{5416}
 	remark := "This is my remark"
 	beego.Trace("Test: ", "DeployContractRequest", "ContributionCodes: ", contributionCodes)
-	gvesList := [9]uint16{29, 0, 18, 21, 0, 0, 5, 9, 1}
-	var a [32]byte
-	copy(a[:], remark)
-	_, _, rc, err := directpaymentrequest.DeployRequestContract(farmerAuth, sim, contributionCodes, a, gvesList)
+	gvesList := []uint16{29, 0, 18, 21, 0, 0, 5, 9, 1}
+
+	_, _, rc, err := directpaymentrequest.DeployRequestContract(farmerAuth, sim, contributionCodes, remark, rbacAddress, gvesList, amountPreviousYear)
 	sim.Commit()
 	requestContract = rc
 	Convey("Subject: Deploy Request-Contract\n", t, func() {
@@ -67,139 +90,264 @@ func TestDeployContract(t *testing.T) {
 		})
 		Convey("Check remark", func() {
 			contract_remark, _ := rc.Remark(nil)
-			So(string(contract_remark[:32]), ShouldContainSubstring, remark)
+			So(contract_remark, ShouldContainSubstring, remark)
 		})
 	})
 
 }
 
-//
-//func TestSetInspector(t *testing.T) {
-//	beego.Trace("Test: ", "SetInspectorId", "InspectorId: ", inspectorAuth.From.String())
-//	_, err := requestContract.SetInspectorId(adminAuth, inspectorAuth.From)
-//	sim.Commit()
-//	Convey("Subject: Set Inspector Id\n", t, func() {
-//		Convey("No error", func() {
-//			So(err, ShouldEqual, nil)
-//		})
-//		Convey("Check Inspector Address", func() {
-//			contract_inspectorId, _ := requestContract.InspectorAddress(nil)
-//			So(inspectorAuth.From, ShouldEqual, contract_inspectorId)
-//		})
-//	})
-//}
-//
-//func _TestAddLacks(t *testing.T) {
-//	beego.Trace("Test: ", "AddLack", "ContributionCode: ", 5416)
-//	_, err := requestContract.AddLack(farmerAuth, 5416, "cat", 1110, "controlPoint", 1, 10)
-//	sim.Commit()
-//	Convey("Subject: Add Inspection Lack with wrong User\n", t, func() {
-//		Convey("No error", func() {
-//			So(err, ShouldEqual, nil)
-//		})
-//		// Only the Inspector is allow to add a Lack
-//		Convey("Number of Lacks should be null", func() {
-//			numLacks, _ := requestContract.NumLacks(nil)
-//			So(numLacks.Cmp(big.NewInt(0)), ShouldEqual, 0) //  Cmp: 0 if x == y
-//		})
-//	})
-//
-//	_, err = requestContract.AddLack(inspectorAuth, 5416, "cat", 1110, "controlPoint", 1, 10)
-//	sim.Commit()
-//	Convey("Subject: Add Inspection Lack as an Inspector\n", t, func() {
-//		Convey("No error", func() {
-//			So(err, ShouldEqual, nil)
-//		})
-//		// Only the Inspector is allow to add a Lack
-//		Convey("Number of Lacks should be null", func() {
-//			numLacks, _ := requestContract.NumLacks(nil)
-//			So(numLacks.Cmp(big.NewInt(0)), ShouldEqual, 1) // +1 if x >  y
-//		})
-//	})
-//}
-//
-//func TestAddNewLacks(t *testing.T) {
-//	beego.Trace("Test: ", "AddLacks", "ContributionCode: ", 5416)
-//	contributionCodes := make([]uint16, 0)
-//	controlCategories := make([]int64, 0)
-//	pointGroupCodes := make([]uint16, 0)
-//	controlPointIds := make([]int64, 0)
-//	lackIds := make([]int64, 0)
-//	points := make([]uint8, 0)
-//
-//	contributionCodes = append(contributionCodes, 5416, 5416)
-//	controlCategories = append(controlCategories, 4, 5)
-//	pointGroupCodes = append(pointGroupCodes, 1110, 1110)
-//	controlPointIds = append(controlPointIds, 3, 4)
-//	lackIds = append(lackIds, 2, 3)
-//	points = append(points, 60, 10)
-//	beego.Debug(contributionCodes)
-//	_, err := requestContract.AddLacks(inspectorAuth, contributionCodes, controlCategories, pointGroupCodes, controlPointIds, lackIds, points)
-//	if (err != nil ) {
-//		beego.Error("Error while adding lacks")
-//		t.Failed()
-//	}
-//	sim.Commit()
-//	Convey("Subject: Add Inspection Lack as an Inspector\n", t, func() {
-//		Convey("No error", func() {
-//			So(err, ShouldEqual, nil)
-//		})
-//		// Only the Inspector is allow to add a Lack
-//		Convey("Number of Lacks should be 1", func() {
-//			numLacks, _ := requestContract.NumLacks(nil)
-//			beego.Debug(requestContract.NumLacks(nil))
-//			So(numLacks.Cmp(big.NewInt(2)), ShouldEqual, 0) //  Cmp: 0 if x == y
-//		})
-//	})
-//}
-//
-//func Test_UpdateBTSGVE(t *testing.T) {
-//	_, err := requestContract.SetGVE(adminAuth, 1, 2, 3, 4, 5, 6, 7, 8, 9)
-//	sim.Commit()
-//	Convey("Subject: Add Inspection Lack with wrong User\n", t, func() {
-//		Convey("No error", func() {
-//			So(err, ShouldEqual, nil)
-//		})
-//		Convey("GVE should be 1", func() {
-//			gve, _ := requestContract.PointGroups(nil, 1110)
-//			So(gve.Gve, ShouldEqual, 1)
-//		})
-//		Convey("GVE should be 2", func() {
-//			gve, _ := requestContract.PointGroups(nil, 1150)
-//			So(gve.Gve, ShouldEqual, 2)
-//		})
-//	})
-//}
-//
-//func _TestCalculateBTS(t *testing.T) {
-//	beego.Trace("Test: ", "Calculate BTS", 5416)
-//
-//	// Add some more lacks
-//	requestContract.AddLack(inspectorAuth, 5416, "cat", 1128, "controlPoint", 2, 110)
-//	requestContract.AddLack(inspectorAuth, 5416, "dog", 1141, "controlPoint", 1, 20)
-//	requestContract.AddLack(inspectorAuth, 5416, "dog", 1141, "controlPoint", 2, 60)
-//	sim.Commit()
-//
-//	_, err := requestContract.CalculateBTS(adminAuth)
-//	if (err != nil) {
-//		beego.Error("Error while calculating BTS")
-//		t.Failed()
-//	}
-//	sim.Commit()
-//
-//	Convey("Subject: Calculate BTS Direct Payment Amount\n", t, func() {
-//		Convey("No error", func() {
-//			So(err, ShouldEqual, nil)
-//		})
-//		Convey("1128 gets no directpayment", func() {
-//			dpVars, _ := requestContract.PointGroups(nil, 1128)
-//			So(dpVars.BtsTotal, ShouldEqual, 0)
-//		})
-//		Convey("1141 should get 297", func() {
-//			dpVars, _ := requestContract.PointGroups(nil, 1141)
-//			total := new(big.Int)
-//			total.Sub(dpVars.BtsTotal, dpVars.BtsDeduction)
-//			So(total.Uint64() / 100, ShouldEqual, 297)
-//		})
-//	})
-//}
+/*
+ Check if GVE is set correctly
+*/
+func Test_GVE(t *testing.T) {
+	Convey("Subject: Check if GVE is set correctly\n", t, func() {
+		Convey("Pointgroup 1110: GVE is 29", func() {
+			cv, _ := requestContract.PointGroups(nil, 1110)
+			So(cv.Gve, ShouldEqual, 29)
+		})
+		Convey("Pointgroup 1150: GVE is 0", func() {
+			cv, _ := requestContract.PointGroups(nil, 1150)
+			So(cv.Gve, ShouldEqual, 0)
+		})
+		Convey("Pointgroup 1128: GVE is 18", func() {
+			cv, _ := requestContract.PointGroups(nil, 1128)
+			So(cv.Gve, ShouldEqual, 18)
+		})
+		Convey("Pointgroup 1141: GVE is 21", func() {
+			cv, _ := requestContract.PointGroups(nil, 1141)
+			So(cv.Gve, ShouldEqual, 21)
+		})
+		Convey("Pointgroup 1142: GVE is 0", func() {
+			cv, _ := requestContract.PointGroups(nil, 1142)
+			So(cv.Gve, ShouldEqual, 0)
+		})
+		Convey("Pointgroup 1124: GVE is 0", func() {
+			cv, _ := requestContract.PointGroups(nil, 1124)
+			So(cv.Gve, ShouldEqual, 0)
+		})
+		Convey("Pointgroup 1129: GVE is 5", func() {
+			cv, _ := requestContract.PointGroups(nil, 1129)
+			So(cv.Gve, ShouldEqual, 5)
+		})
+		Convey("Pointgroup 1143: GVE is 9", func() {
+			cv, _ := requestContract.PointGroups(nil, 1143)
+			So(cv.Gve, ShouldEqual, 9)
+		})
+		Convey("Pointgroup 1144: GVE is 1", func() {
+			cv, _ := requestContract.PointGroups(nil, 1144)
+			So(cv.Gve, ShouldEqual, 1)
+		})
+	})
+}
+
+/*
+ User from canton sets inspector
+*/
+func Test_SetInspector(t *testing.T) {
+	beego.Trace("Test: ", "SetInspector ", "Inspector address: ", inspectorAuth.From.String())
+	_, err := requestContract.SetInspectorId(cantonAuth, inspectorAuth.From)
+	sim.Commit()
+	Convey("Subject: Set Inspector Id\n", t, func() {
+		Convey("No error", func() {
+			So(err, ShouldEqual, nil)
+		})
+		Convey("Check Inspector Address", func() {
+			contract_inspectorId, _ := requestContract.InspectorAddress(nil)
+			So(inspectorAuth.From, ShouldEqual, contract_inspectorId)
+		})
+	})
+}
+
+/*
+ Inspect sets lacks
+*/
+func Test_AddLacks(t *testing.T) {
+	cCodes := []uint16{5416, 5416, 5416, 5416, 5416, 5417, 5417, 5417, 5417, 5417}
+	cCatIds := []int64{1, 1, 1, 1, 1, 2, 2, 2, 2, 2}
+	pgCodes := []uint16{1110, 1110, 1128, 1129, 1144, 1110, 1110, 1128, 1129, 1144}
+	cPoiIds := []int64{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	lackIds := []int64{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	points := []uint8{40, 20, 111, 0, 80, 40, 20, 111, 0, 80}
+
+	_, err := requestContract.AddLacks(fakeInspectorAuth, cCodes, cCatIds, pgCodes, cPoiIds, lackIds, points)
+	sim.Commit()
+	Convey("Subject: Add Inspection Lack with fake Inspector\n", t, func() {
+		Convey("No error", func() {
+			So(err, ShouldEqual, nil)
+		})
+		// Only the assigned Inspector is allow to add a Lack
+		Convey("Number of Lacks should be null", func() {
+			numLacks, _ := requestContract.NumLacks(nil)
+			So(numLacks.Cmp(big.NewInt(0)), ShouldEqual, 0) //  Cmp: 0 if x == y
+		})
+	})
+
+	_, err = requestContract.AddLacks(inspectorAuth, cCodes, cCatIds, pgCodes, cPoiIds, lackIds, points)
+
+	sim.Commit()
+	Convey("Subject: Add Inspection Lack as an Inspector\n", t, func() {
+		Convey("No error", func() {
+			So(err, ShouldEqual, nil)
+		})
+		Convey("Number of Lacks should be null", func() {
+			numLacks, _ := requestContract.NumLacks(nil)
+			So(numLacks.Cmp(big.NewInt(0)), ShouldEqual, 1) // +1 if x >  y
+		})
+	})
+}
+
+/*
+ Test if 'minus' points are stored
+*/
+func Test_PointGroupCalculation(t *testing.T) {
+	Convey("Subject: Check if point are added to PointGroupCalculation struct\n", t, func() {
+		Convey("Pointgroup 1110: BTS Point is 60", func() {
+			pgc, _ := requestContract.PointGroups(nil, 1110)
+			So(pgc.BtsPoints, ShouldEqual, 60)
+		})
+		Convey("Pointgroup 1128: BTS Point is 111", func() {
+			pgc, _ := requestContract.PointGroups(nil, 1128)
+			So(pgc.BtsPoints, ShouldEqual, 111)
+		})
+		Convey("Pointgroup 1144: BTS Point is 80", func() {
+			pgc, _ := requestContract.PointGroups(nil, 1144)
+			So(pgc.BtsPoints, ShouldEqual, 80)
+		})
+		Convey("Pointgroup 1110: RAUS Point is 60", func() {
+			pgc, _ := requestContract.PointGroups(nil, 1110)
+			So(pgc.RausPoints, ShouldEqual, 60)
+		})
+		Convey("Pointgroup 1128: RAUS Point is 111", func() {
+			pgc, _ := requestContract.PointGroups(nil, 1128)
+			So(pgc.RausPoints, ShouldEqual, 111)
+		})
+		Convey("Pointgroup 1144: RAUS Point is 80", func() {
+			pgc, _ := requestContract.PointGroups(nil, 1144)
+			So(pgc.RausPoints, ShouldEqual, 80)
+		})
+	})
+}
+
+/*
+ Test if BTS Amount and deduction has been calculated correctly
+*/
+func Test_CalculateBTS(t *testing.T) {
+	Convey("Subject: Check if total amount and deduction is calculated correctly for BTS\n", t, func() {
+		_, err := requestContract.CalculateBTS(cantonAuth)
+		sim.Commit()
+		Convey("No error", func() {
+			So(err, ShouldEqual, nil)
+		})
+		Convey("Pointgroup 1110: Amount is 261000", func() {
+			cv, _ := requestContract.PointGroups(nil, 1110)
+			So(cv.BtsTotal.Cmp(big.NewInt(261000)), ShouldEqual, 0)
+		})
+		Convey("Pointgroup 1110: Deduction is 4500", func() {
+			cv, _ := requestContract.PointGroups(nil, 1110)
+			So(cv.BtsDeduction.Cmp(big.NewInt(4500)), ShouldEqual, 0)
+		})
+		Convey("Pointgroup 1128: Amount is 162000", func() {
+			cv, _ := requestContract.PointGroups(nil, 1128)
+			So(cv.BtsTotal.Cmp(big.NewInt(162000)), ShouldEqual, 0)
+		})
+		Convey("Pointgroup 1128: Deduction is 162000", func() {
+			cv, _ := requestContract.PointGroups(nil, 1128)
+			So(cv.BtsDeduction.Cmp(big.NewInt(162000)), ShouldEqual, 0)
+		})
+		Convey("Pointgroup 1129: Amount is 45000", func() {
+			cv, _ := requestContract.PointGroups(nil, 1129)
+			So(cv.BtsTotal.Cmp(big.NewInt(45000)), ShouldEqual, 0)
+		})
+		Convey("Pointgroup 1129: Deduction is 0", func() {
+			cv, _ := requestContract.PointGroups(nil, 1129)
+			So(cv.BtsDeduction.Cmp(big.NewInt(0)), ShouldEqual, 0)
+		})
+		Convey("Pointgroup 1144: Amount is 0, (not qualified)", func() {
+			cv, _ := requestContract.PointGroups(nil, 1144)
+			So(cv.BtsTotal.Cmp(big.NewInt(0)), ShouldEqual, 0)
+		})
+		Convey("Pointgroup 1144: Deduction is 0", func() {
+			cv, _ := requestContract.PointGroups(nil, 1144)
+			So(cv.BtsDeduction.Cmp(big.NewInt(0)), ShouldEqual, 0)
+		})
+	})
+}
+
+/*
+ Test if RAUS Amount and deduction has been calculated correctly
+*/
+func Test_CalculateRAUS(t *testing.T) {
+	Convey("Subject: Check if total amount and deduction is calculated correctly for RAUS\n", t, func() {
+		_, err := requestContract.CalculateRAUS(cantonAuth)
+		sim.Commit()
+		Convey("No error", func() {
+			So(err, ShouldEqual, nil)
+		})
+		Convey("Pointgroup 1110: Amount is 551000", func() {
+			cv, _ := requestContract.PointGroups(nil, 1110)
+			So(cv.RausTotal.Cmp(big.NewInt(551000)), ShouldEqual, 0)
+		})
+		Convey("Pointgroup 1110: Deduction is 9500", func() {
+			cv, _ := requestContract.PointGroups(nil, 1110)
+			So(cv.RausDeduction.Cmp(big.NewInt(9500)), ShouldEqual, 0)
+		})
+		Convey("Pointgroup 1128: Amount is 342000", func() {
+			cv, _ := requestContract.PointGroups(nil, 1128)
+			So(cv.RausTotal.Cmp(big.NewInt(342000)), ShouldEqual, 0)
+		})
+		Convey("Pointgroup 1128: Deduction is 342000", func() {
+			cv, _ := requestContract.PointGroups(nil, 1128)
+			So(cv.RausDeduction.Cmp(big.NewInt(342000)), ShouldEqual, 0)
+		})
+		Convey("Pointgroup 1129: Amount is 95000", func() {
+			cv, _ := requestContract.PointGroups(nil, 1129)
+			So(cv.RausTotal.Cmp(big.NewInt(95000)), ShouldEqual, 0)
+		})
+		Convey("Pointgroup 1129: Deduction is 0", func() {
+			cv, _ := requestContract.PointGroups(nil, 1129)
+			So(cv.RausDeduction.Cmp(big.NewInt(0)), ShouldEqual, 0)
+		})
+		Convey("Pointgroup 1144: Amount is 37000", func() {
+			cv, _ := requestContract.PointGroups(nil, 1144)
+			So(cv.RausTotal.Cmp(big.NewInt(37000)), ShouldEqual, 0)
+		})
+		Convey("Pointgroup 1144: Deduction is 25900", func() {
+			cv, _ := requestContract.PointGroups(nil, 1144)
+			So(cv.RausDeduction.Cmp(big.NewInt(25900)), ShouldEqual, 0)
+		})
+	})
+}
+
+/*
+ Test if amout of first payout is correctly
+*/
+func Test_FirstPayoutAmount(t *testing.T) {
+	payoutAmount := big.NewInt(0)
+	payoutAmount.Div(amountPreviousYear, big.NewInt(2))
+	Convey("Subject: Get amount for first payment\n", t, func() {
+		amount, err := requestContract.GetFirstPaymentAmount(nil)
+		Convey("No error", func() {
+			So(err, ShouldEqual, nil)
+		})
+		Convey("First payment should be "+payoutAmount.String(), func() {
+			So(amount.Cmp(payoutAmount), ShouldEqual, 0)
+		})
+
+	})
+}
+
+/*
+ Test if amout of final payout is correctly
+*/
+func Test_FinalPayoutAmount(t *testing.T) {
+	Convey("Subject: Get amount for final payment\n", t, func() {
+		amount, err := requestContract.GetFinalPaymentAmount(nil)
+		Convey("No error", func() {
+			So(err, ShouldEqual, nil)
+		})
+		Convey("First payment should be ", func() {
+			So(amount.Cmp(big.NewInt(1539100)), ShouldEqual, 0)
+		})
+
+	})
+}
